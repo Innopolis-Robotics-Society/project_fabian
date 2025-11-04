c328f2f346612c9d0eca1a0583615f01183d7373
otkisaev <polarevia@bk.ru>
2025-11-04 18:57:07 +0300

    fixed camera and a lot

--- f_bringup/launch/tf.launch.py
+++ f_bringup/launch/tf.launch.py
@@ -16,7 +16,7 @@ def static_tf(name, x,y,z, r,p,yaw, parent, child):
 def generate_launch_description():
     tf_map_to_odom = static_tf('tf_map_to_odom', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 'map', 'odom')
     tf_odom_to_base = static_tf('tf_odom_to_base', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 'odom', 'base_link')
-    tf_base_to_camera = static_tf('tf_base_to_camera', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 'base_link', 'camera_link')
+    tf_base_to_camera = static_tf('tf_base_to_camera', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 'base_link', 'camera')
     return LaunchDescription([
         tf_odom_to_base,
         tf_map_to_odom,
